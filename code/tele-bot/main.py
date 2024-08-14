35fecc43389fa6f40681bbfbb565e32b7a526eb3
Maxim Gushchin <mag-email@ya.ru>
2024-08-14 22:39:44 +0300

    Стратегический коммит

--- code/tele-bot/main.py
+++ code/tele-bot/main.py
@@ -69,8 +69,8 @@ def save_btn(call):
     bot.send_message(message.chat.id, name_get[1])
 
     roles = ['Воин', 'Маг', 'Клерик', 'Разбойник', 'Бард', 'Паладин', 'Друид', 'Варвар', 'Плут', 'Монах']
-    player[0] = f"{name_get[0]} это {random.choice(roles, k=1)}"
-    player[1] = f"{name_get[1]} это {random.choice(roles, k=1)}"
+    player[0] = f"{name_get[0]} это {random.choice(roles)}"
+    player[1] = f"{name_get[1]} это {random.choice(roles)}"
     bot.send_message(message.chat.id, player[0])
     bot.send_message(message.chat.id, player[1])
 
